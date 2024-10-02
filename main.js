72477b4d9764f787c8d8a317c8968c643ee41ee7
zekt1209 <victor_27edge@hotmail.com>
2024-10-02 11:58:02 -0600

    Creacion del proyecto

---
+++ main.js
@@ -0,0 +1,29 @@
+console.log('Hello World');
+
+
+// import ServiceNowChat from "https://dev266073.service-now.com/uxasset/externals/now-requestor-chat-popover-app/index.jsdbx?sysparm_substitute=false";
+// var chat = new ServiceNowChat({ instance: "https://dev266073.service-now.com", });
+
+/* var chat = new ServiceNowChat({
+	instance: 'https://dev266073.service-now.com',
+	context: {
+		skip_load_history: 1
+	},
+	branding: {
+		bgColor: '#333',
+		primaryColor: '#000',
+		hoverColor: '#EFEFEF',
+		activeColor: '#AAA',
+		openIcon: 'custom-open.svg',
+		closeIcon: 'custom-close.svg',
+		sizeMultiplier: 1.6
+	},
+	offsetX: 50,
+	offsetY: 50,
+	position: 'left',
+	translations: {
+		'Open dialog': 'Open chat',
+		'Open Chat. {0} unread message(s)': 'Click to open',
+		'Close chat.': 'Click to close',
+	},
+}); */
