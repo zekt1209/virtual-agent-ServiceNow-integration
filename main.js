2b27580822853f5872995a87ab3d8176fa374307
zekt1209 <victor_27edge@hotmail.com>
2024-11-19 12:51:40 -0600

    Last version

--- main.js
+++ main.js
@@ -1,29 +1,36 @@
 console.log('Hello World');
 
 
-// import ServiceNowChat from "https://dev266073.service-now.com/uxasset/externals/now-requestor-chat-popover-app/index.jsdbx?sysparm_substitute=false";
+import ServiceNowChat from "https://bccrdev.service-now.com/uxasset/externals/now-requestor-chat-popover-app/index.jsdbx?sysparm_substitute=false";
 // var chat = new ServiceNowChat({ instance: "https://dev266073.service-now.com", });
 
-/* var chat = new ServiceNowChat({
-	instance: 'https://dev266073.service-now.com',
+ var chat = new ServiceNowChat({
+	instance: 'https://bccrdev.service-now.com',
 	context: {
-		skip_load_history: 1
+		// skip_load_history: 1,
+		branding_key: "scp_bccr_branding",
+		skip_load_history: false,
+		load_active_only: true,
+		portal: 'scp',
+		live_agent_queue: 'e25db237c301565004c8322c0501315d',
+		default_topic: 'b6512af8c3599a90df41e7deb00131ac',
+		language: 'es',
 	},
-	branding: {
-		bgColor: '#333',
-		primaryColor: '#000',
-		hoverColor: '#EFEFEF',
-		activeColor: '#AAA',
-		openIcon: 'custom-open.svg',
-		closeIcon: 'custom-close.svg',
-		sizeMultiplier: 1.6
-	},
-	offsetX: 50,
-	offsetY: 50,
-	position: 'left',
+/* 	branding: {
+		// bgColor: '#008cff',
+		// primaryColor: '#ffefd5',
+		// hoverColor: '#1b6e97',
+		// activeColor: '#AAA',
+		// openIcon: 'custom-open.svg',
+		// closeIcon: 'custom-close.svg',
+		// sizeMultiplier: 1.6
+	}, */
+	offsetX: 25,
+	offsetY: 15,
+	position: 'right',
 	translations: {
 		'Open dialog': 'Open chat',
 		'Open Chat. {0} unread message(s)': 'Click to open',
 		'Close chat.': 'Click to close',
 	},
-}); */
+}); 
